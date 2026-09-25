911bbc720776ced1a7bbdc37a2919e265bfa37b5
Krishna Shaw <supportveexorakt@gmail.com>
2026-09-26 02:23:51 +0530

    Add sitemap

---
+++ app/sitemap.ts/page.tsx
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://kt-veexora.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://kt-veexora.vercel.app/tools",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: "https://kt-veexora.vercel.app/contact",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+    {
+      url: "https://kt-veexora.vercel.app/privacy",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+  ];
+}
